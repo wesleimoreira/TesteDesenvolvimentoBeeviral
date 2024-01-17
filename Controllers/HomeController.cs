1551e3571e097f3a92e62633efbca963accf05fa
wesleimoreira <wesleimoreira600@gmail.com>
2024-01-17 15:16:00 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteDesenvolvimentoBeeviral.Models;
+
+namespace TesteDesenvolvimentoBeeviral.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static readonly List<Pessoa> Pessoas = new();
+
+        [HttpGet]
+        public IActionResult Index() => View();        
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CalculoFatorial([FromForm] string numero)
+        {
+            if (!string.IsNullOrEmpty(numero) && numero.All(char.IsDigit))
+            {
+                if (int.TryParse(numero, out int valor) && valor > 0)
+                {
+                    var resultado = 1;
+
+                    for (int n = 1; n <= valor; n++)
+                    {
+                        resultado *= n;
+                    }
+
+                    ViewData["Numero"] = resultado;
+                }
+                else
+                {
+                    ViewData["Erro"] = "Digite um número inteiro não negativo e maior que zero!";
+                }
+            }
+            else
+            {
+                ViewData["Erro"] = "Digite um número inteiro não negativo e maior que zero!";
+            }
+
+            return View(nameof(Index));
+        }
+
+
+        [HttpGet]
+        public IActionResult CadastroPessoa()
+        {
+            if (Pessoas.Count > 0)
+                ViewData["Pessoas"] = Pessoas.Where(x => x.Idade > 30).ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CadastroEListaDePessoas([FromForm] Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                Pessoas.Add(pessoa);
+
+                ViewData["Pessoas"] = Pessoas.Where(x => x.Idade > 30).ToList();
+            }
+
+            return View(nameof(CadastroPessoa));
+        }
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TesteDesenvolvimentoBeeviral.Models
+{
+    public class Pessoa
+    {
+        public Pessoa() { }
+        public Pessoa(string nome, int idade, string email)
+        {
+            Nome = nome;
+            Idade = idade;
+            Email = email;
+        }
+
+        [Required(ErrorMessage = "O Nome e Obrigatório!")]
+        public string Nome { get; set; } = default!;
+
+        [Required(ErrorMessage = "A Idade e obrigatórioa!")]
+        public int Idade { get; set; } = default!;
+
+        [Required(ErrorMessage = "Email Obrigatório!")]
+        [EmailAddress(ErrorMessage = "Digite um email valido!")]
+        public string Email { get; set; } = default!;
+    }
+}
